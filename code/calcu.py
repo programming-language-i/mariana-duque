3a2cfc5db798430ef094d0f4898ac0bbe027e317
duquemariana29 <marianaceballos2927@gmail.com>
2026-09-17 20:16:28 -0500

    cambios en el archivo calcu.py con operaciones

---
+++ code/calcu.py
@@ -0,0 +1,17 @@
+def suma(valor1, valor2):
+    return valor1 + valor2
+
+def resta(valor1, valor2):
+    return valor1 - valor2
+
+def multiplicacion(valor1, valor2):
+    return valor1 * valor2
+
+resultado_suma = suma(2, 5)
+resultado_resta = resta(2, 5)
+resultado_multiplicacion = multiplicacion(2, 5)
+
+print(resultado_suma)
+print(resultado_resta)
+print(resultado_multiplicacion)
+
